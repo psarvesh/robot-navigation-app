2f7117bbd087743ff2b94f2ad715c3802c7f7bcb
Sarvesh Patkar <psarvesh314@gmail.com>
2021-09-05 13:22:47 +0530

    Made head and tail atomic for correctness

--- app/include/ring_buffer.h
+++ app/include/ring_buffer.h
@@ -1,10 +1,15 @@
 #ifndef __APP_RING_BUFFER__
 #define __APP_RING_BUFFER__
 
+// C header files
+#include <stdatomic.h>
+
+// POSIX system headers
 #include <pthread.h>
 typedef struct  {
     char *buffer;
-    int capacity, head, tail;
+    int capacity;
+    atomic_int head, tail; // Should be atomic to ensure partial reads do not affect execution
 } RingBuffer;
 
 int InitBuffer(RingBuffer *rb, int max_size);
