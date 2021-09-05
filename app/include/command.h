ba020e11a9e027407c3b4b64cf42e76fcbfda15b
Sarvesh Patkar <psarvesh314@gmail.com>
2021-09-05 11:38:44 +0530

    Remove Command specific code out of main

---
+++ app/include/command.h
@@ -0,0 +1,60 @@
+#ifndef __APP_COMMAND__
+#define __APP_COMMAND__
+
+// C header files
+#include <stdio.h>
+
+#define max_cmd_size  8  
+
+typedef enum {
+    kNorth,
+    kSouth,
+    kEast,
+    kWest, 
+    kExit, 
+    kInvalid
+} CommandType;
+
+void ParseCommand(const char *str, CommandType *type, int *n) {
+    
+    *type = kInvalid;
+    *n = 0;
+    
+    if(strcmp(str, "+++EXIT;") == 0) { // Special case
+        *type = kExit;
+        fprintf(stdout, "\noutput> Exited");
+    } else if(!(str[0] == '+' && str[1] == '+' && str[2] == '+' && str[3] == 'M' && 
+            (str[4] == 'N' || str[4] == 'S' || str[4] == 'E' || str[4] == 'W') && 
+            isdigit(str[5]) && isdigit(str[6]) && str[max_cmd_size - 1] ==';')) {
+        // Invalid case
+        fprintf(stdout, "\noutput> Invalid command\ninput> ");
+    } else {
+        // Valid command
+        *n = (10 * (str[5] - '0')) + (str[6] - '0');
+        switch (str[4]) {
+            case 'N' : 
+                *type = kNorth;
+                fprintf(stdout, "\noutput> Moved north %d steps\ninput> ", *n);
+                break;
+            case 'S' : 
+                *type = kSouth;
+                fprintf(stdout, "\noutput> Moved south %d steps\ninput> ", *n);
+                break;
+            case 'E' : 
+                *type = kEast; 
+                fprintf(stdout, "\noutput> Moved east %d steps\ninput> ", *n);
+                break;
+            case 'W' : 
+                *type = kWest; 
+                fprintf(stdout, "\noutput> Moved west %d steps\ninput> ", *n);
+                break;
+            default:
+                // Should not reach here
+                *type = kInvalid; 
+                fprintf(stdout, "\noutput> Invalid command\ninput> ");
+                break;
+        }
+    }
+}
+
+#endif // __APP_COMMAND__
--- app/src/main.c
+++ app/src/main.c
@@ -13,19 +13,10 @@
 
 // Project header files
 #include "ring_buffer.h"
-#include "threads.h"
+#include "command.h"
 
-static volatile bool exitFlag = false;
-#define max_cmd_size  8      
+static volatile bool exitFlag = false;    
 static char cmd[max_cmd_size + 1]; // +++<4 CHARS>;
-typedef enum {
-    kNorth,
-    kSouth,
-    kEast,
-    kWest, 
-    kExit, 
-    kInvalid
-} CommandType;
 
 // Producer thread will be here
 void *Producer(void *userdata) {
@@ -42,7 +33,7 @@ void *Producer(void *userdata) {
     return NULL;
 }
 
-/* static void InitStdIO(struct termios *attr) {
+static void InitStdIO(struct termios *attr) {
     setbuf(stdout, NULL);
     struct termios new_attr;
     tcgetattr(STDIN_FILENO, attr);
@@ -54,44 +45,21 @@ void *Producer(void *userdata) {
 
 static void RestoreStdIO(struct termios *attr) {
     tcsetattr(STDIN_FILENO, TCSANOW, attr);
-} */
-
-static void ParseCommand(const char *str, CommandType *type, int *n) {
-    *type = kInvalid;
-    *n = 0;
-    if(strcmp(str, "+++EXIT;") == 0) {
-        *type = kExit;
-    } else if(!(str[0] == '+' && str[1] == '+' && str[2] == '+' && str[3] == 'M' && 
-            (str[4] == 'N' || str[4] == 'S' || str[4] == 'E' || str[4] == 'W') && 
-            isdigit(str[5]) && isdigit(str[6]) && str[max_cmd_size - 1] ==';')) {
-        return;
-    } else {
-        // Valid command
-        *n = (10 * (str[5] - '0')) + (str[6] - '0');
-        switch (str[4]) {
-            case 'N' : *type = kNorth; break;
-            case 'S' : *type = kSouth; break;
-            case 'E' : *type = kEast; break;
-            case 'W' : *type = kWest; break;
-            default: *type = kInvalid; break;
-        }
-    }
-}
+} 
 
 // main will act as the main consumer thread
 int main(int argc, char** argv) {
-    int ret = 0;
-//    struct termios attr;
-    setbuf(stdout, NULL);
     if(argc != 3) {
         fprintf(stderr, "Usage: ./robot <max size> <time>\n");
         fprintf(stderr, "<max size> = Max size of the buffer in characters\n");
         fprintf(stderr, "<time> = Time interval in seconds at which consumer wakes up to read\n");
         exit(EXIT_FAILURE);
     }
-    
+    int ret = 0;
+    struct termios attr;
+
     // Turn off Canonical mode
-//    InitStdIO(&attr);
+    InitStdIO(&attr);
 
     // Initialize Buffer
     RingBuffer rb;
@@ -101,7 +69,6 @@ int main(int argc, char** argv) {
         exit(EXIT_FAILURE);
     }
 
-    fprintf(stdout, "input> ");
     // Create producer
     pthread_t producer;
     ret = pthread_create(&producer, NULL, Producer, &rb);
@@ -110,6 +77,7 @@ int main(int argc, char** argv) {
         exit(EXIT_FAILURE);
     }
 
+    // Consumer thread
     int sleep_time = atoi(argv[2]);
     char c = 0;
     int pos = 0;
@@ -123,44 +91,22 @@ int main(int argc, char** argv) {
                 CommandType type;
                 int n;
                 ParseCommand(cmd, &type, &n);
-                switch(type) {
-                    case kNorth:
-                        fprintf(stdout, "\noutput> Moved north %d steps\n", n);
-                        break;
-                    case kSouth:
-                        fprintf(stdout, "\noutput> Moved south %d steps\n", n);
-                        break;
-                    case kWest:
-                        fprintf(stdout, "\noutput> Moved west %d steps\n", n);
-                        break;
-                    case kEast:
-                        fprintf(stdout, "\noutput> Moved east %d steps\n", n);
-                        break;
-                    case kExit:
-                        fprintf(stdout, "\noutput> Exited");
-                        exitFlag = true;
-                        pthread_cancel(producer);
-                        break;
-                    case kInvalid:
-                    default:
-                        fprintf(stdout, "\noutput> Invalid command\n");
-                        break;
-                }
-                memset(cmd, 0, max_cmd_size + 1);
-                if(exitFlag) {
+                if(type == kExit) {
+                    exitFlag = true;
+                    pthread_cancel(producer);
                     break;
                 }
-                fprintf(stdout, "input> ");
+                memset(cmd, 0, max_cmd_size + 1);
                 pos = 0; 
             } 
         }
     }
     
     // Clean up
-    void *res;
+    void *res; // For debugging purposes
     pthread_join(producer, &res);
     FreeBuffer(&rb);
     fprintf(stdout, "\n<terminated>\n");
-//    RestoreStdIO(&attr);
+    RestoreStdIO(&attr);
     return EXIT_SUCCESS;
 }
